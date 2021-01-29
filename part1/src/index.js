a825bd0461126945ad1bacee69fe45f7922bc485
nnguerin <63073129+nnguerin@users.noreply.github.com>
2021-01-29 10:39:34 -0800

    using dictionaries instead of setting each var

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,59 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Header = (props) => {
-	return (
-		<h1>
-			{props.course}
-		</h1>
-	)
+const Header = ({ course }) => {
+  return (
+    <h1>{course.name}</h1>
+  )
+}
+
+const Total = ({ course }) => {
+  const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+  return(
+    <p>Number of exercises {sum}</p>
+  ) 
 }
 
 const Part = (props) => {
-	return (
-		<p>
-			{props.name} {props.exercises}
-		</p>
-	)
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>    
+  )
 }
 
-const Content = (props) => {
-	return (
-		<div>
-			<Part name = {props.part1} exercises = {props.exercises1}/>
-			<Part name = {props.part2} exercises = {props.exercises2}/>
-			<Part name = {props.part3} exercises = {props.exercises3}/>
-		</div>
-	)
-}
-
-const Total = (props) => {
-	return (
-		<p>
-			Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
-		</p>
-	)
+const Content = ({ course }) => {
+  return (
+    <div>
+      <Part part={course.parts[0]} />
+      <Part part={course.parts[1]} />
+      <Part part={course.parts[2]} />
+    </div>
+  )
 }
 
 const App = () => {
-	const course = 'Half Stack application development'
-	const part1 = 'Fundamentals of React'
-	const exercises1 = 10
-	const part2 = 'Using props to pass data'
-	const exercises2 = 7
-	const part3 = 'State of a component'
-	const exercises3 = 14
-  
-	return (
-	  <div>
-		<Header course={course} />
-		<Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} 
-		part3={part3} exercises3={exercises3} />
-		<Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-	  </div>
-	)
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   }
 
-  // testbranch comment
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  )
+}
 
-  ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
